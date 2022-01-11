71aad3564a554722b818891d06e7adfb1fbd5201
juanma <juxmix@gmail.com>
2022-01-11 17:34:56 +0100

    Corrected many add_word errors Added logging Ignored \ufeff Unicode notepad indicator

--- CompactDictionary.py
+++ CompactDictionary.py
@@ -1,18 +1,29 @@
+import logging
+
 class CompactDictionary:
 
     def __init__(self, language="English"):
+        self.END_WORD = '-'
         self.language = language
         self.fullDict = dict()
+        logging.basicConfig(level=logging.INFO)
 
     def clean_word(self, word):
         return word.strip("\t\n .,;:&/()[]{}¡!¿?$€-_\"\'0123456789")
 
     def add_word(self, word):
-        clean_word1 = self.clean_word(word)
-        for letter in clean_word1:
-            sub_dict = self.fullDict[letter]
-            if sub_dict is None:
-                sub_dict = {letter: None}
+        clean_word1 = self.clean_word(word).lower()
+        if (clean_word1 is None) or (len(clean_word1) < 1):
+            return False
+        first_letter = clean_word1[0]
+        if first_letter not in self.fullDict:
+            self.fullDict[first_letter] = dict()
+        sub_dict = self.fullDict.get(first_letter)
+        for letter in clean_word1[1:]:
+            if sub_dict is None or letter not in sub_dict:
+                sub_dict[letter] = {}
+            sub_dict = sub_dict[letter]
+        return True
 
     def contains(self, word):
         resto = self.fullDict.get(word[0])
@@ -28,11 +39,14 @@ class CompactDictionary:
         return True
 
     def load_text(self, file_name):
-        f = open(file_name, 'rU')
+        f = open(file_name)
         for line in f:
             lst_pals = line.split()
             for palabra in lst_pals:
-                self.add_word(palabra)
+                if '\ufeff' in palabra:
+                    continue
+                added = self.add_word(palabra)
+                logging.debug(palabra + " " + str(added))
         f.close()
 
 
@@ -40,8 +54,13 @@ class CompactDictionary:
 
 if __name__ == '__main__':
     cd = CompactDictionary()
-    clean_word = cd.cleanword(" \n$0_algo-[]123.;\t ")
-    print("Clean word: '{}'".format(clean_word))
+    clean_word = cd.clean_word(" \n$0_algo-[]123.;\t ")
+    logging.info("Clean word: '{}'".format(clean_word))
+    cd.load_text("Frankenstein.txt")
+    words = ("ancien", "monster", "lady", "gentleman")
+    for word in words:
+        is_in = cd.contains(word)
+        logging.info(word + " in Frankenstein.txt: " + str(is_in))
         
 
 
