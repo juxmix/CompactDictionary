893b1201a3cb5e7e2b08fb6bd16c5fd64be17263
Juanma Sánchez <juxmix@gmail.com>
2022-01-13 15:16:24 +0100

    Added .gitignore for generated debug text files Added debug test files for inserted, rejected and failed words upon insertion Added free english books to test the program Minor modifications to load process with word cleaning

--- CompactDictionary.py
+++ CompactDictionary.py
@@ -1,4 +1,9 @@
 import logging
+import re
+
+F_INSERTED_WORDS = "Inserted.txt"
+F_REJECTED_WRODS = "Rejected.txt"
+F_FAILED_WORDS = "Failed.txt"
 
 class CompactDictionary:
 
@@ -9,11 +14,13 @@ class CompactDictionary:
         logging.basicConfig(level=logging.INFO)
 
     def clean_word(self, word):
-        return word.strip("\t\n .,;:&/()[]{}¡!¿?$€-_\"\'0123456789")
+        return word.strip("\t\n .,;:&#*/()[]{}¡!¿?$€—-_“”\‘\"\'0123456789").lower()
 
     def add_word(self, word):
-        clean_word1 = self.clean_word(word).lower()
+        clean_word1 = self.clean_word(word)
         if (clean_word1 is None) or (len(clean_word1) < 1):
+            return False
+        if self.contains(clean_word1):
             return False
         first_letter = clean_word1[0]
         if first_letter not in self.fullDict:
@@ -21,16 +28,20 @@ class CompactDictionary:
         sub_dict = self.fullDict.get(first_letter)
         for letter in clean_word1[1:]:
             if sub_dict is None or letter not in sub_dict:
-                sub_dict[letter] = {}
+                sub_dict[letter] = dict()
             sub_dict = sub_dict[letter]
         return True
 
     def contains(self, word):
-        resto = self.fullDict.get(word[0])
+        clean_word = word.lower()
+        #clean_word = self.clean_word(word)
+        #if (not clean_word is None) and (len(clean_word) > 0):
+        #    return False
+        resto = self.fullDict.get(clean_word[0])
         keys = self.fullDict.keys()
-        if not word[0] in keys:
+        if not clean_word[0] in keys:
             return False
-        for letter in word[1:]:
+        for letter in clean_word[1:]:
             keys = resto.keys()
             if letter in keys:
                 resto = resto.get(letter)
@@ -40,25 +51,43 @@ class CompactDictionary:
 
     def load_text(self, file_name):
         f = open(file_name)
+        fi = open(F_INSERTED_WORDS, "w")
+        fr = open(F_REJECTED_WRODS, "w")
+        ff = open(F_FAILED_WORDS, "w")
         num_words = 0
         skipped_words = 0
         failed_words = 0
         for line in f:
-            lst_pals = line.split()
+            #lst_pals = line.split()
+            lst_pals = re.split(':|;|_|,|\.|/|\’|\t| |\'|\-|\—|\n', line)
             for palabra in lst_pals:
                 #Notepad UTF-8 unicode indicator breaks everything
                 if '\ufeff' in palabra:
+                    fr.write(palabra + " (Marca de Notepad de UTF-8)")
                     continue
-                if not self.contains(palabra):
+                added = False
+                if len(palabra) > 0 and not self.contains(palabra):
                     added = self.add_word(palabra)
-                    num_words = num_words + 1 if added else num_words
-                    failed_words = failed_words + 1 if not added else failed_words
+                    if added:
+                        num_words += 1
+                        fi.write(str(num_words) + " " +\
+                                 palabra + " " + self.clean_word(palabra) + "\n")
+                    else:
+                        failed_words += 1
+                        ff.write(str(failed_words) + " " +\
+                                 palabra + " " + self.clean_word(palabra) + "\n")
                 else:
                     skipped_words += 1
+                    fr.write(str(skipped_words) + " " +\
+                             palabra + " " + self.clean_word(palabra) + "\n")
+
                 logging.debug(palabra + " " + str(added))
         logging.info("Added: " + str(num_words) + " / skipped: " + str(skipped_words)\
                      + " / failed: " + str(failed_words))
         f.close()
+        ff.close()
+        fr.close()
+        fi.close()
         
 
 
--- main.py
+++ main.py
@@ -3,10 +3,31 @@ import logging
 
 if __name__ == '__main__':
     cd = compDict.CompactDictionary()
-    clean_word = cd.clean_word(" \n$0_algo-[]123.;\t ")
-    logging.info("Clean word: '{}'".format(clean_word))
-    cd.load_text("Frankenstein.txt")
-    cd.load_text("Pride_and_prejudice.txt")
+    #clean_word = cd.clean_word(" \n$0_algo-[]123.;\t ")
+    #logging.info("Clean word: '{}'".format(clean_word))
+    books = ("Frankenstein", "Pride_and_prejudice", "The_Scarlet_Letter", "Alice_in_Wonderland", \
+             "Sherlock_Holmes", "Chrismas_Carol", "A_Doll's_house", "The_Sun_Also_Rises", \
+             "A_Modest_Proposal", "The_Great_Gatsby", "A_Tale_of_two_cities", \
+             "The_importance_of_being_ernest", "Moby_Dick", "The_Picture_of_Dorian_Grey")
+    for title in books:
+        full_book_name = "Books/" + title + ".txt"
+        logging.info("--------------")
+        logging.info("Loading: " + full_book_name)
+        cd.load_text(full_book_name)
+    logging.info("---------------")
+    '''    
+    cd.load_text("Books/Frankenstein.txt")
+    cd.load_text("Books/Pride_and_prejudice.txt")
+    cd.load_text("Books/The_Scarlet_Letter.txt")
+    cd.load_text("Books/Alice_in_Wonderland.txt")
+    cd.load_text("Books/Sherlock_Holmes.txt")
+    cd.load_text("Books/Chrismas_Carol.txt")
+    cd.load_text("Books/A_Doll's_house.txt")
+    cd.load_text("Books/The_Sun_Also_Rises.txt")
+    cd.load_text("Books/A_Modest_Proposal.txt")
+    cd.load_text("Books/The_Great_Gatsby.txt")
+    cd.load_text("Books/A_Tale_of_two_cities.txt")
+    '''
     words = ("ancien", "monster", "lady", "gentleman")
     for word in words:
         is_in = cd.contains(word)
